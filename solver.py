c1b59f4c0d12588fe97aca34add70e9a5a0b68fd
Harm Campmans <Harm.Campmans@sioux.eu>
2021-02-23 22:17:51 +0100

    filling in some edges for demo purposes

--- solver.py
+++ solver.py
@@ -5,7 +5,11 @@ if __name__ == "__main__":
              [None, 0, None, None],
              [3, 3, None, 2],
              [None, None, 2, 2]]
-    _board = Board(table) #if I would not type the underscore python might maybe confuse the object with a class or definition.
-    # You cant call an object list, but you can call it _list
+    _board = Board(table)
+    for edge in _board.edges:
+        source = edge.source.pos()
+        dest = edge.dest.pos()
+        if source[0] % 3 == 0 and dest[1] % 3 == 2:
+            edge.make()
     _board.print()
     print("breakpoint here in Pycharm")
