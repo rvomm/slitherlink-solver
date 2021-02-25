73760b1dc7beccc300ba4a983ec1af3c9d963bc3
Harm Campmans <Harm.Campmans@sioux.eu>
2021-02-25 22:28:37 +0100

    let's cut out the crap. It started doing stuff on it's own!

--- solver.py
+++ solver.py
@@ -6,11 +6,6 @@ if __name__ == "__main__":
              [3, 3, None, 2],
              [None, None, 2, 2]]
     _board = Board(table)
-    for edge in _board.edges:
-        source = edge.source.pos()
-        dest = edge.dest.pos()
-        if source[0] % 3 == 0 and dest[1] % 3 == 2:
-            edge.make()
     _board.print()
 
     print("___________________________________")
@@ -24,7 +19,6 @@ if __name__ == "__main__":
         for square in _board.squares:
             square.update()
         new_unknown_count = _board.unknonw_edge_count()
-    # _board.structures[2].update()
     _board.print()
 
 
