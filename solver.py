aafb80a28f0aec87d9318d9b836fa3e51b27859f
robertvommeren <40067215+robertvommeren@users.noreply.github.com>
2021-02-27 15:37:52 +0100

    align solver with Game class

--- solver.py
+++ solver.py
@@ -1,8 +1,9 @@
-from Board import Board
-import level_loader
+import game 
+
 if __name__ == "__main__":
-    table = level_loader.load_level_list()[2]
-    _board = Board(table)
+    level = "test1.txt"
+    _board = game.Game(level).board
+    
     new_unknown_count = _board.unknonw_edge_count()
     old_unknown_count = new_unknown_count + 1
     while new_unknown_count < old_unknown_count:
