a8015f34de56c56cd927456cb2f4afd9f4c0cf24
Harm Campmans <Harm.Campmans@sioux.eu>
2021-02-27 10:53:53 +0100

    debugging new level. Solver made mistake

--- solver.py
+++ solver.py
@@ -1,7 +1,7 @@
 from Board import Board
 import level_loader
 if __name__ == "__main__":
-    table = level_loader.load_level_list()[1]
+    table = level_loader.load_level_list()[2]
     _board = Board(table)
     new_unknown_count = _board.unknonw_edge_count()
     old_unknown_count = new_unknown_count + 1
