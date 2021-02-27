289bd58c7ace0dac6a94e77bb101bc18c8bd1fc3
robertvommeren <40067215+robertvommeren@users.noreply.github.com>
2021-02-27 15:37:34 +0100

    create new Game class that contains Board

---
+++ game.py
@@ -0,0 +1,25 @@
+
+
+import Board
+import re
+
+class Game: 
+    
+    def __init__(self, path):
+        self.path = path
+        self.board = Board.Board(self._constraints())
+
+    def _constraints(self):
+
+        # TODO: add validations
+        return self._read_constraints()
+    
+    def _read_constraints(self):
+        
+        with open(self.path, "r") as file:
+            level = file.read().split("\n")
+
+        level = [re.sub("[^01234]", " ", line) for line in level]
+        constraints = [[None if x == " " else int(x) for x in list(line)] for line in level]
+    
+        return constraints
